0869dbd543d44dd9ef71d2dd8a345bfea11ef875
Zubaidov <shohruz.zubaidov@gmail.com>
2023-08-21 18:14:08 +0400

    The errors tooken into consideration

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, 
@@ -19,7 +19,14 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
     context = {
         'posts' : posts
         }

6c933f2187b19484417f6a3ad99e48a3d29780e9
Zubaidov <shohruz.zubaidov@gmail.com>
2023-08-22 14:33:31 +0400

    Email is ready

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+env = environ.Env()
+environ.Env.read_env() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +127,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'zbdtechpro@gmail.com'
+EMAIL_HOST_PASSWORD = env('PASS')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
